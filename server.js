aaa828c3576f888cff069cb1c9d5c7301a292b29
Ivan Orellana Jr <iorellana21@gmail.com>
2021-02-27 18:41:39 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// generate a webpage that displays teams basic info
+// start by asking team manager prompts
+// then ask about team members - either an engineer or an intern
+    // if engineer, ask for Role; if intern, ask for school
+// after each team member creation, ask until team has been completed
+
+// install node/npm
+// install inquirer for user prompts
+// install jest for test classes
+
+
